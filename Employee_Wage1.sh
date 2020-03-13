ff25f780b48e2fc8916f523789ce7bac0904b45f
sagar-528 <gupta.sagar528@gmail.com>
2020-03-13 10:14:44 +0530

    ADDED: Fiveth cases

--- Employee_Wage1.sh
+++ Employee_Wage1.sh
@@ -2,18 +2,22 @@
 
 isPartTime=1
 isFullTime=2
+totalSalary=0
 empRatePerHr=20
-randomCheck=$(( RANDOM % 3 ))
+numWorkingDays=20
 
-case $empCheck in
-		$isFullTime)
+for (( day=1; day<=$numWorkingDays; day++))
+do
+	empCheck=$(( RANDOM % 3 ))
+		case $empCheck in
+			$isFullTime)
 				empHrs=8 ;;
-
-		$isPartTime)
+			$isPartTime)
 				empHrs=4 ;;
+			*)
+				empHrs=0 ;;
+		esac
 
-		*)
-		empHrs=0 ;;
-esac
-
-salary=$(( $empHrs * $empRatePerHr ))
+	salary=$(( $empHrs * $empRatePerHr ))
+	totalSalary=$(( $totalSalary + $salary))
+done
