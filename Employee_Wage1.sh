4b712297cbeaf71f9f141e15fb49a005caaed067
sagar-528 <gupta.sagar528@gmail.com>
2020-03-14 17:01:14 +0530

    ADDED:Eight cases

--- Employee_Wage1.sh
+++ Employee_Wage1.sh
@@ -3,12 +3,12 @@
 # CONSTANTS FOR THE PROGRAM
 isPartTime=1
 isFullTime=2
-maxHrsInMonth=4
+maxHrsInMonth=10
 empRatePerHr=20
 numWorkingDays=20
 
 # VARIABLES
-totalEmpHr=0
+totalWorkHours=0
 totalWorkingDays=0
 
 function getWorkingHours() {
@@ -23,11 +23,19 @@ function getWorkingHours() {
 	echo $workHours
 }
 
+function calcDailyWage(){
+local workHrs=$1
+wage=$(($workHrs * $empRatePerHr))
+echo $wage 
+}
+
 while [[ $totalWorkHours -lt $maxHrsInMonth && 
 			$totalWorkingDays -lt $numWorkingDays ]]
 do
 	((totalWorkingDays++))
 	workHours="$( getWorkingHours $((RANDOM%3)) )"
 	totalWorkHours=$(($totalWorkHours+$workHours))
+	empDailyWage[$totalWorkingDays]="$( calcDailyWage $workHours )"
 done
-	totalSalary=$(($totalWorkHours*$empRatePerHr))
+	totalSalary="$( calcDailyWage $totalWorkHours )"
+	echo "Daily Wage" ${empDailyWage[@]}
